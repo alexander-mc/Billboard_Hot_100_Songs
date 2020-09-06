4919f77e4ede71bb33d435645493c85ce926ff59
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-09-06 16:14:45 -0700

    revert to original environment

--- lib/billboard_hot_100.rb
+++ lib/billboard_hot_100.rb
@@ -1,14 +1,14 @@
 module BillboardHot100
 end
 
-require_relative '../config/environment'
+# require_relative '../config/environment'
 
-# require 'pry'
-# require 'nokogiri'
-# require 'open-uri'
-# require 'colorize'
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
 
-# require_relative './billboard_hot_100/song.rb'
-# require_relative './billboard_hot_100/scraper.rb'
-# require_relative './billboard_hot_100/cli.rb'
-# require_relative './billboard_hot_100/version.rb'
+require_relative './billboard_hot_100/song.rb'
+require_relative './billboard_hot_100/scraper.rb'
+require_relative './billboard_hot_100/cli.rb'
+require_relative './billboard_hot_100/version.rb'
